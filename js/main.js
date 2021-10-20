42d75fd4df1e5fd0f7208c566a439545ceca4c96
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2021-10-21 04:52:07 +0530

    completed website

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// active navbar
+let nav = document.querySelector(".navgation-wrap");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+
+// NAV HIDE
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
